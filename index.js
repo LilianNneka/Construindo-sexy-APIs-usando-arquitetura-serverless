b8e89c62eed4bedfc0e417c6b83ab72bc91af51a
Lilian Nneka <72230179+LilianNneka@users.noreply.github.com>
2021-10-27 16:32:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv/config')
+const mongoose = require('mongoose')
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB Error', err))
+
+const connection = mongoose.connection
+
+module.exports = connection
+© 2021 GitHub, Inc.
